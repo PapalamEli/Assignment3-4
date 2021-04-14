c55b6847ce613c1e39b19c3db0cdd5fe4b9f602a
Papalam.eli <47798778+PapalamEli@users.noreply.github.com>
2021-04-14 00:25:48 -0500

    Add files via upload

--- welcome/tests.py
+++ welcome/tests.py
@@ -1,8 +1,13 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, LiveServerTestCase
 from django.urls import reverse, resolve, path
 from welcome.forms import BMIForms, RetirementForms
 from welcome.views import welcome_page, BMI, retirement
 from django.http import JsonResponse, HttpResponseBadRequest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Create your tests here.
 
@@ -15,16 +20,6 @@ class TestForms(TestCase):
             'weight': '120.1'
         })
         self.assertTrue(form.is_valid())
-
-    def test_BMI_calc(self):
-        response = self.client.get(reverse('welcome:bmi_cal'), {
-            'first': 1,
-            'second': 1,
-        })
-        self.assertEqual(response.status_code, 200)
-        self.assertEquals(200, {
-            'results': '200',
-        })
 
     def test_BMI_no_data(self):
         form = BMIForms(data={})
@@ -101,3 +96,48 @@ class TestUrls(TestCase):
     def test_retire_url(self):
         url = reverse('welcome:retire')
         self.assertEquals(resolve(url).func, retirement)
+
+class TestFrontEnd(LiveServerTestCase):
+
+    def testBMI(self):
+        selenium = webdriver.Firefox()
+        selenium.get('http://127.0.0.1:8000/bmi_calculator')
+
+        feet = selenium.find_element_by_id('id_feet')
+        inches = selenium.find_element_by_id('id_inches')
+        weight = selenium.find_element_by_id('id_weight')
+
+        submit = selenium.find_element_by_name('submit')
+
+        feet.send_keys('5')
+        inches.send_keys('4')
+        weight.send_keys('120')
+
+        #submit.send_keys(Keys.RETURN)
+        submit.click()
+        selenium.implicitly_wait(10)
+        assert 'BMI is 21.1 - Normal Weight' in selenium.page_source
+        
+
+
+    def testRetire(self):
+        selenium = webdriver.Firefox()
+        selenium.get('http://127.0.0.1:8000/retirement_calculator')
+
+        age = selenium.find_element_by_id('id_age')
+        salary = selenium.find_element_by_id('id_salary')
+        savings = selenium.find_element_by_id('id_savings')
+        goal = selenium.find_element_by_id('id_goal')
+
+        submit = selenium.find_element_by_name('submit')
+
+        age.send_keys('20')
+        salary.send_keys('50000')
+        savings.send_keys('0.25')
+        goal.send_keys('120000')
+
+        #submit.send_keys(Keys.RETURN)
+        submit.click()
+
+        assert 'Goal will be met at age: 27' in selenium.page_source
+        
